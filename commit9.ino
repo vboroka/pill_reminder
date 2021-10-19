2b081d06ea058614e6b742bce4be5664597344c9
v.boro <73988372+vboroka@users.noreply.github.com>
2021-10-19 16:42:38 +0200

    Pulzus-szenzor tesztelése

---
+++ commit9.ino
@@ -0,0 +1,40 @@
+
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>  
+
+//  Variables
+const int PulseWire = 0;
+int Threshold = 550;
+                               
+PulseSensorPlayground pulseSensor;
+
+
+void setup() {   
+
+  Serial.begin(9600);
+
+ 
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED13);
+  pulseSensor.setThreshold(Threshold);   
+
+   if (pulseSensor.begin()) {
+    Serial.println("PulseSensor objektum létrehozva!");
+  }
+}
+
+
+
+void loop() {
+
+ int myBPM = pulseSensor.getBeatsPerMinute();  
+
+
+if (pulseSensor.sawStartOfBeat()) {
+ Serial.print("BPM: ");
+ Serial.println(myBPM);
+}
+
+  delay(20);
+
+} 
